aba7fc21bc385ed9108a0bbf0eff2950203f10a2
smadinen <51313354+smadinen@users.noreply.github.com>
2021-03-01 20:29:25 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ConvivaSDK",
-            url: "https://github.com/Conviva/ConvivaSDK/raw/master/Framework/ConvivaSDK.xcframework.zip",
+            url: "https://github.com/Conviva/ConvivaSDK/raw/4.0.10/Framework/ConvivaSDK.xcframework.zip",
             checksum: "ca0b36f01d9349c8c18c219701076a52b26aff5f86a8d9435b15bd555197ed00")
     ]
 )
